0364682320d5ae19c499801f319d50c44bda3461
bbvch13531 <bbvch13531@gmail.com>
2018-12-28 14:16:16 +0900

    Add navigation bar
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- app/ppoggle/TimerController.swift
+++ app/ppoggle/TimerController.swift
@@ -10,10 +10,12 @@ import Foundation
 import UIKit
 import UICircularProgressRing
 
+
 class TimerController:UIViewController{
 	//	required init?(coder aDecoder: NSCoder) {
 	//		super.init(nibName: nil, bundle: nil)
 	//	}
+	
 	var frame : CGRect{
 		get{
 			return CGRect(x: 50, y: 100, width: 300, height: 300)
@@ -23,21 +25,11 @@ class TimerController:UIViewController{
 	
 	override func viewDidLoad() {
 		self.title = "Timer"
-	
-		let startBtn = UIButton(frame: CGRect(x: 50, y: 450, width: 120, height: 50))
-		startBtn.setTitle("start", for: .normal)
-		startBtn.backgroundColor = UIColor.orange
-		let resetBtn = UIButton(frame: CGRect(x: 200, y: 450, width: 120, height: 50))
-		resetBtn.setTitle("reset", for: .normal)
 		
+		setUpNavigationBar()
+		setUpProgressRing()
+		setUpBtns()
 		
-		self.view.addSubview(startBtn)
-		self.view.addSubview(resetBtn)
-		startBtn.addTarget(self, action: #selector(self.startBtnClick), for: .touchDown)
-		resetBtn.addTarget(self, action: #selector(self.resetBtnClick), for: .touchDown)
-		self.view.addSubview(progressRing)
-		start()
-	
 		//: Animate with a given duration
 		//
 		//		//: Pause and continue animations dynamically
@@ -58,16 +50,22 @@ class TimerController:UIViewController{
 		//		//: For more information [read the docs](https://goo.gl/JJCHeo)
 		
 	}
-	@objc func startBtnClick(_ sender: UIButton?){
-		self.progressRing.startProgress(to: 100, duration: 2){
-			print("Done!!")
-		}
-	}
-	@objc func resetBtnClick(_ sender: UIButton?){
-		self.progressRing.resetProgress()
-	}
-	func start(){
+	private func setUpNavigationBar(){
+		let screenRect = UIScreen.main.bounds
+		let screenWidth = screenRect.size.width
+		let screenHeight = screenRect.size.height
 		
+		let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 50, width: screenWidth, height: 70))
+		self.view.addSubview(navBar);
+		
+		let navItem = UINavigationItem(title: "SomeTitle");
+		let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: nil, action: nil);
+		navItem.leftBarButtonItem = doneItem;
+		
+		navBar.setItems([navItem], animated: false);
+		
+	}
+	private func setUpProgressRing(){
 		progressRing.backgroundColor = .white
 		progressRing.outerRingColor = .blue
 		progressRing.innerRingColor = .white
@@ -82,12 +80,33 @@ class TimerController:UIViewController{
 		
 		// This is called when it's finished animating!
 		
-//		DispatchQueue.main.async {
-//			// We can animate the ring back to another value here, and it does so fluidly
-//			self.progressRing.startProgress(to: 100, duration: 2){
-//				print("Done!!")
-//			}
-//		}
+		//		DispatchQueue.main.async {
+		//			// We can animate the ring back to another value here, and it does so fluidly
+		//			self.progressRing.startProgress(to: 100, duration: 2){
+		//				print("Done!!")
+		//			}
+		//		}
+		self.view.addSubview(progressRing)
+	}
+	private func setUpBtns(){
+		let startBtn = UIButton(frame: CGRect(x: 50, y: 450, width: 120, height: 50))
+		startBtn.setTitle("start", for: .normal)
+		startBtn.backgroundColor = UIColor.orange
+		let resetBtn = UIButton(frame: CGRect(x: 200, y: 450, width: 120, height: 50))
+		resetBtn.setTitle("reset", for: .normal)
+		resetBtn.backgroundColor = UIColor.orange
 		
+		self.view.addSubview(startBtn)
+		self.view.addSubview(resetBtn)
+		startBtn.addTarget(self, action: #selector(self.startBtnClick), for: .touchDown)
+		resetBtn.addTarget(self, action: #selector(self.resetBtnClick), for: .touchDown)
+	}
+	@objc func startBtnClick(_ sender: UIButton?){
+		self.progressRing.startProgress(to: 100, duration: 2){
+			print("Done!!")
+		}
+	}
+	@objc func resetBtnClick(_ sender: UIButton?){
+		self.progressRing.resetProgress()
 	}
 }

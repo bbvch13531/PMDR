676eeaa67890c2391fc04238628151b05fd2a93b
bbvch13531 <bbvch13531@gmail.com>
2018-12-28 13:26:48 +0900

    Add start and reset button
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- app/ppoggle/TimerController.swift
+++ app/ppoggle/TimerController.swift
@@ -11,47 +11,63 @@ import UIKit
 import UICircularProgressRing
 
 class TimerController:UIViewController{
-//	required init?(coder aDecoder: NSCoder) {
-//		super.init(nibName: nil, bundle: nil)
-//	}
-	let frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-
+	//	required init?(coder aDecoder: NSCoder) {
+	//		super.init(nibName: nil, bundle: nil)
+	//	}
+	var frame : CGRect{
+		get{
+			return CGRect(x: 50, y: 100, width: 300, height: 300)
+		}
+	}
+	lazy var progressRing = UICircularProgressRing(frame: frame)
+	
 	override func viewDidLoad() {
 		self.title = "Timer"
-//		self.view.backgroundColor = UIColor.orange
+	
+		let startBtn = UIButton(frame: CGRect(x: 50, y: 450, width: 120, height: 50))
+		startBtn.setTitle("start", for: .normal)
+		startBtn.backgroundColor = UIColor.orange
+		let resetBtn = UIButton(frame: CGRect(x: 200, y: 450, width: 120, height: 50))
+		resetBtn.setTitle("reset", for: .normal)
 		
+		
+		self.view.addSubview(startBtn)
+		self.view.addSubview(resetBtn)
+		startBtn.addTarget(self, action: #selector(self.startBtnClick), for: .touchDown)
+		resetBtn.addTarget(self, action: #selector(self.resetBtnClick), for: .touchDown)
+		self.view.addSubview(progressRing)
 		start()
+	
 		//: Animate with a given duration
+		//
+		//		//: Pause and continue animations dynamically
+		//		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+		//			progressRing.pauseProgress()
+		//			//: Customize the ring even more
+		//			progressRing.showsValueKnob = true
+		//			progressRing.valueKnobSize = 20
+		//			progressRing.valueKnobColor = .green
+		//
+		//		}
+		//
+		//		//: Continue the animation whenever you want
+		//		DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+		//			progressRing.continueProgress()
+		//		}
+		//
+		//		//: For more information [read the docs](https://goo.gl/JJCHeo)
 		
-//		{
-//			// This is called when it's finished animating!
-//
-//			DispatchQueue.main.async {
-//				// We can animate the ring back to another value here, and it does so fluidly
-//				progressRing.startProgress(to: 80, duration: 2)
-//			}
-//		}
-//
-//		//: Pause and continue animations dynamically
-//		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//			progressRing.pauseProgress()
-//			//: Customize the ring even more
-//			progressRing.showsValueKnob = true
-//			progressRing.valueKnobSize = 20
-//			progressRing.valueKnobColor = .green
-//
-//		}
-//
-//		//: Continue the animation whenever you want
-//		DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-//			progressRing.continueProgress()
-//		}
-//
-//		//: For more information [read the docs](https://goo.gl/JJCHeo)
-
+	}
+	@objc func startBtnClick(_ sender: UIButton?){
+		self.progressRing.startProgress(to: 100, duration: 2){
+			print("Done!!")
+		}
+	}
+	@objc func resetBtnClick(_ sender: UIButton?){
+		self.progressRing.resetProgress()
 	}
 	func start(){
-		let progressRing = UICircularProgressRing(frame: frame)
+		
 		progressRing.backgroundColor = .white
 		progressRing.outerRingColor = .blue
 		progressRing.innerRingColor = .white
@@ -59,9 +75,19 @@ class TimerController:UIViewController{
 		progressRing.innerRingWidth = 8
 		progressRing.ringStyle = .ontop
 		progressRing.font = UIFont.boldSystemFont(ofSize: 40)
-		self.view.addSubview(progressRing)
-		progressRing.startProgress(to: 100, duration: 3){
-			print("Done!!")
-		}
+		
+		//		progressRing.startProgress(to: 100, duration: 3){
+		//			print("Done!!")
+		//		}
+		
+		// This is called when it's finished animating!
+		
+//		DispatchQueue.main.async {
+//			// We can animate the ring back to another value here, and it does so fluidly
+//			self.progressRing.startProgress(to: 100, duration: 2){
+//				print("Done!!")
+//			}
+//		}
+		
 	}
 }

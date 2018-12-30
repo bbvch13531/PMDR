bea212e5c94c437b9cf4157168a6bce286604784
bbvch13531 <bbvch13531@gmail.com>
2018-12-30 16:08:11 +0900

    Complete tabbar images
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- app/ppoggle/PGTabBarController.swift
+++ app/ppoggle/PGTabBarController.swift
@@ -13,13 +13,19 @@ class PGTabBarController : UITabBarController{
 	
 	override func viewDidLoad() {
 		let timerController = TimerController()
+		let mypomoController = MypomoController()
+		let reportController = ReportController()
+		let userGroupController = UserGroupController()
 		let settingController = SettingController()
-		self.viewControllers = [timerController,settingController]
+		
+		self.viewControllers = [timerController,mypomoController,reportController,userGroupController,settingController]
 //		self.view.backgroundColor = UIColor.red
 //		timerController.tabBarItem = UITabBarItem( ,tag: 0)
-		timerController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home-selected-29"), tag: 0)
-		settingController.tabBarItem = UITabBarItem(title: "설정", image: UIImage(named: "userGroup"), tag:1)
-		
+		timerController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home"), tag: 0)
+		mypomoController.tabBarItem = UITabBarItem(title: "내 뽀모도로", image: UIImage(named:"document"), tag: 1)
+		reportController.tabBarItem = UITabBarItem(title: "리포트", image: UIImage(named: "asending-arrow"), tag: 2)
+		userGroupController.tabBarItem = UITabBarItem(title: "함께하기", image: UIImage(named: "userGroup"), tag: 3)
+		settingController.tabBarItem = UITabBarItem(title: "설정", image: UIImage(named: "setting"), tag:4)
 		
 	}
 }

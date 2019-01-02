c76ea57addbcd6bda3dff13e61020e0c622aa16f
bbvch13531 <bbvch13531@gmail.com>
2019-01-02 21:17:53 +0900

    Limit cell number by add footer
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- app/ppoggle/SettingController.swift
+++ app/ppoggle/SettingController.swift
@@ -9,27 +9,91 @@
 import Foundation
 import UIKit
 
-class SettingController: UITableViewController {
+class SettingController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 //	required init?(coder aDecoder: NSCoder) {
 //		super.init(nibName: nil, bundle: nil)
 //	}
+	var tableView: UITableView = UITableView()
 	
-	override func viewDidLoad() {
-		
-	}
+	lazy var list:[String] = {
+		var titleData = [String]()
+		return titleData
+	}()
+	
 	
 	/// view가 appear될 때마다 실행되는 함수.
 	/// tableView의 data를 reload함
 	/// - Parameter animated: animated: Bool
-	override func viewDidAppear(_ animated: Bool) {
-		self.tableView.reloadData()
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		let screenSize = UIScreen.main.bounds
+		let screenWidth = screenSize.size.width
+		let screenHeight = screenSize.size.height
+		
+		tableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+		tableView.delegate = self
+		tableView.dataSource = self
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "settingCell")
+		
+		self.view.addSubview(tableView)
+		
+		let footerView = UIView(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+		footerView.backgroundColor = UIColor.green
+		tableView.tableFooterView = footerView
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.setUpList()
+//		DispatchQueue.global().async{
+//			defer{
+//				DispatchQueue.main.async {
+//					self.tableView.reloadData()
+//				}
+//			}
+//			do{
+//				// 리스트 초기화
+//				self.setUpList()
+//			}catch{
+//				print(error.localizedDescription)
+//			}
+//		}
 	}
 	
+	
+	
+	
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//		let row = self.list[indexPath.row]
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
+//		cell.settingName = "test"
+//
+//		var sbtn = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+//		sbtn.setTitle(">", for: .normal)
+//
+//		cell.settingBtn = sbtn
+//		NSLog("indexPath.row = %d, cell.name = %s",indexPath.row, cell.settingName)
+		
+		for title in self.list {
+			cell.textLabel!.text = self.list[indexPath.row]
+		}
+		return cell
+	}
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		<#code#>
+		return 50
 	}
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		<#code#>
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 6
 	}
 	
+	func setUpList(){
+		let titleArr = ["프로필 수정","알림","서비스 약관","개인정보 처리방침","로그아웃","탈퇴하기"]
+		
+		for title in titleArr {
+			self.list.append(title)
+		}
+		
+	}
 }
---
+++ app/ppoggle/SettingVO.swift
@@ -0,0 +1,14 @@
+//
+//  SettingVO.swift
+//  ppoggle
+//
+//  Created by KyungYoung Heo on 02/01/2019.
+//  Copyright © 2019 KyungYoung Heo. All rights reserved.
+//
+
+import Foundation
+
+class SettingVO: Codable{
+	var title: String?
+	var controlTtype: String?
+}

a066898c2acb5f2b8a5f37ab2bf10a3042fa6184
bbvch13531 <bbvch13531@gmail.com>
2020-02-11 21:45:12 +0900

    Add animation
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- app/ppoggle/Controllers/ReportController.swift
+++ app/ppoggle/Controllers/ReportController.swift
@@ -41,12 +41,54 @@ class ReportController: UIViewController {
     $0.layer.borderWidth = 1
   }
   
+  var bar1 = UIView().then {
+    $0.frame = CGRect(x: 50, y: 300, width: 20, height: 30)
+    $0.backgroundColor = PGColors.red
+  }
+  
+  var bar2 = UIView().then {
+    $0.frame = CGRect(x: 50, y: 360, width: 20, height: 30)
+    $0.backgroundColor = PGColors.yellow
+  }
+  
+  var rect2 = UIView().then {
+    $0.frame = CGRect(x: 50, y: 350, width: 200, height: 200)
+    $0.backgroundColor = PGColors.red
+  }
+  
 	override func viewDidLoad() {
     self.view.addSubview(horizontalLine_1)
     self.view.addSubview(horizontalLine_2)
     self.view.addSubview(horizontalLine_3)
     self.view.addSubview(titleLabel)
     self.view.addSubview(todayLabel)
+    self.view.addSubview(bar1)
+    self.view.addSubview(bar2)
+    self.view.addSubview(rect2)
+    
+    let options: UIView.AnimationOptions = [.curveEaseInOut]
+    
+    UIView.animate(withDuration: 1.0,
+                         delay: 0,
+                         options: options,
+                         animations: { [weak self] in
+                          self?.bar1.frame.size.width *= 10.0
+//                          self?.bar2.frame.size.width *= 7.0
+            }, completion: nil)
+    
+    UIView.animate(withDuration: 0.5,
+                 delay: 0,
+                 options: options,
+                 animations: { [weak self] in
+//                  self?.bar1.frame.size.width *= 10.0
+                  self?.bar2.frame.size.width *= 10.0
+    }, completion: nil)
+    
+    rect2.transform = CGAffineTransform(scaleX: 0, y: 0)
+
+    UIView.animate(withDuration: 1, animations: {
+        self.rect2.transform = .identity
+    }, completion: nil)
+    
 	}
-  
 }
